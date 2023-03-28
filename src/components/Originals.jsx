4b9721349a97b2bdbe7fdd1fa1067e6771b8cf9b
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-03-28 15:10:13 +0530

    changed styling of the movies container

--- src/components/Originals.jsx
+++ src/components/Originals.jsx
@@ -24,22 +24,21 @@ const Originals = (props) => {
     </Container>
   );
 };
-
 const Container = styled.div`
-padding: 0 0 26px;
-margin: 60px 30px;
-`
+  padding: 0 0 26px;
+  margin: 60px 10px;
+`;
 
 const Content = styled.div`
-display: grid;
-gap: 25px;
-grid-gap: 25px;
-grid-template-columns: repeat(4, minmax(0, 1fr) );
+  display: grid;
+  grid-gap: 25px;
+  gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+`;
 
-@media (max-width: 768px){
-    grid-template-columns: repeat(2, minmax(0,1fr));
-}
-`
 const Wrap = styled.div`
   padding-top: 56.25%;
   border-radius: 10px;
@@ -50,33 +49,25 @@ const Wrap = styled.div`
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
-  
-  
-  img{
-      inset: 0px;
+  img {
+    inset: 0px;
     display: block;
     height: 100%;
     object-fit: cover;
     opacity: 1;
     position: absolute;
-    width: 100%;
     transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
     z-index: 1;
-    
-    &:hover{
-        border-radius: 12px;
-        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+    top: 0;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-    transform: scale(1.01);
-    }
-
-    &:hover{
-    border: 5px solid rgba(249, 249, 249, 0.8);
-    }
-}
-
-
-`
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
 
 
 
